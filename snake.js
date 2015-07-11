c96c6840c1320f505e3d825b1d8774aeda23a292
Alex Luecke <me@alexjl.com>
2015-07-11 15:38:32 -0700

    Init.

---
+++ snake.js
@@ -0,0 +1,190 @@
+
+var Snake = function() {
+
+	var self = this;
+	self.dir = 'right';
+	self.score = 0;
+	self.loop = null;
+
+	self.key = { LEFT: '37', UP: '38', RIGHT: '39', DOWN: '40', };
+
+	self.objs = {
+		food: null,
+		snake: [],
+		canvas: $("#canvas")[0],
+	};
+
+	self.parms = {
+		size: 10,
+		slen: 5,
+		scolor: '#eeeeee',
+		ccolor: '#444444',
+		tcolor: '#ffffff',
+		time: {
+			interval: 100,
+			default: 100,
+			speed_up: 5,
+		}
+	}
+
+	self.context = self.objs.canvas.getContext("2d");
+	self.w = self.objs.canvas.width || window.innerWidth;
+	self.h = self.objs.canvas.height || window.innerHeight;
+
+	self.init = function() {
+			//We will add another clause to prevent reverse gear
+		self.setup_key_events();
+		self.reset();
+		self.run();
+	}
+
+	self.run = function() {
+		self.loop = setInterval(function() {
+			self.update_objects();
+			self.env.render();
+		}, self.parms.time.interval);
+	}
+
+	self.reset = function() {
+		if (typeof self.loop !== "undefined")
+			clearInterval(self.loop);
+		self.score = 0;
+		self.dir = 'right';
+		self.parms.time.interval = self.parms.time.default;
+		self.create_snake();
+		self.create_food();
+	}
+
+	self.setup_key_events = function() {
+		$(document).keydown(function(e){
+			var key = e.which;
+			if (key == self.key.LEFT && self.dir != 'right')
+				self.dir = 'left';
+			else if (key == self.key.UP && self.dir != 'down')
+				self.dir = 'up';
+			else if (key == self.key.RIGHT && self.dir != 'left')
+				self.dir = 'right';
+			else if (key == self.key.DOWN && self.dir != 'up')
+				self.dir = 'down';
+		})
+	}
+
+	self.create_food = function() {
+		var sz = self.parms.size;
+		self.objs.food = {
+			x: Math.round(Math.random()*(self.w-sz)/sz),
+			y: Math.round(Math.random()*(self.h-sz)/sz),
+		};
+	}
+
+	self.create_snake = function() {
+		// Each snake part has an x,y coordinate
+		self.objs.snake = [];
+		for (var i=self.parms.slen-1; i >= 0; i--) {
+			self.objs.snake.push({x: i, y: 2});
+		}
+	}
+
+	self.update_timer = function() {
+		if (typeof self.loop != "undefined")
+			clearInterval(self.loop);
+		self.run();
+	}
+
+	self.update_objects = function() {
+		var head = self.objs.snake[0];
+		var nx = head.x
+			, ny = head.y;
+
+		// Increment the head position
+		if (self.dir === 'up') ny--;
+		else if (self.dir === 'right') nx++;
+		else if (self.dir === 'down') ny++;
+		else if (self.dir === 'left') nx--;
+
+		// If snake body is off the canvas, restart.
+		if (nx < 0 || nx >= self.w/self.parms.size
+				|| ny < 0 || ny >= self.h/self.parms.size
+				|| self.env.check_collision(nx, ny, self.objs.snake)) {
+			self.reset();
+			self.run();
+			return;
+		}
+
+		// If the snake eats a food, it grows.
+		self.objs.snake.unshift({ x: nx, y: ny });
+		if (nx == self.objs.food.x && ny == self.objs.food.y) {
+			var t = self.parms.time.interval;
+			self.create_food();
+			self.parms.time.interval = (t <= 20) ? t : t-self.parms.time.speed_up;
+			self.update_timer();
+			self.score++;
+		} else {
+			self.objs.snake.pop();
+		}
+
+		// TODO: Change this to only render changed cells rather than reflowing the
+		// entire canvas. I don't quite know how I want to do this yet so I am
+		// defering for now.
+		self.env.render();
+	}
+
+	self.env = {
+
+		check_collision: function(x, y, arr) {
+			for (var i = 0; i < arr.length; i++) {
+				if (arr[i].x == x && arr[i].y == y)
+					return true;
+			}
+			return false;
+		},
+
+		render: function() {
+			var sz = self.parms.size;
+			self.context.fillStyle = self.parms.ccolor;
+			self.context.fillRect(0, 0, self.w, self.h);
+			self.context.strokeStyle = "black";
+			self.context.strokeRect(0, 0, self.w, self.h);
+
+			// Draw the snake and food
+			self.env.draw_snake();
+			self.env.draw_square(self.objs.food.x, self.objs.food.y, '#ff4444');
+
+			// Show the score
+			self.context.fillStyle = self.parms.tcolor;
+			self.context.fillText(self.score, sz/2, sz);
+		},
+
+		draw_snake: function() {
+			var len = self.objs.snake.length
+				, s = self.objs.snake;
+			for (var i=0; i <  len; i++) {
+				self.env.draw_square(s[i].x, s[i].y, self.parms.scolor);
+			}
+		},
+
+		draw_circle: function(x, y, radius, color) {
+			self.context.beginPath();
+			self.context.arc(x, y, radius, 0, 2*Math.PI);
+			self.context.fill();
+		},
+
+		draw_square: function(x, y, color) {
+			var sz = self.parms.size;
+			self.context.fillStyle = color;
+			self.context.fillRect(x*sz, y*sz, sz, sz);
+		},
+
+		draw_barriers: function() {
+		}
+
+	}
+
+	return { init: self.init, start: self.run }
+
+};
+
+$(document).ready(function(){
+	var g = new Snake();
+	g.init();
+})
